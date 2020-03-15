ea5ace400ae248cda6e294b96cfb4276a70e47ce
inspirit941 <inspirit941@gmail.com>
2020-03-15 20:15:11 +0900

    template

--- djangoWeb/settings.py
+++ djangoWeb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.path.join(BASE_DIR, 'secrets.json')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
@@ -133,6 +133,6 @@ STATICFILES_DIRS = (
 django_heroku.settings(locals())
 
 # Heroku: Update database configuration from $DATABASE_URL. 
-import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500) 
-DATABASES['default'].update(db_from_env)
+# import dj_database_url 
+# db_from_env = dj_database_url.config(conn_max_age=500) 
+# DATABASES['default'].update(db_from_env)
